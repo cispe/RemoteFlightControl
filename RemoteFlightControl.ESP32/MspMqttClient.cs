20716a93ee1878f967d4234c04dc757cb82c10b3
cispe <cispe@CISPE-DESKTOP>
2025-06-20 16:09:08 +0300

    Reducing 'ESP32' project, because of little amount of RAM

--- RemoteFlightControl.ESP32/MspMqttClient.cs
+++ RemoteFlightControl.ESP32/MspMqttClient.cs
@@ -2,9 +2,10 @@
 
 using System;
 using System.Text;
+using System.IO.Ports;
 using System.Threading;
-using System.Collections;
 using nanoFramework.M2Mqtt;
+using nanoFramework.Hardware.Esp32;
 using nanoFramework.M2Mqtt.Messages;
 using System.Diagnostics.CodeAnalysis;
 using System.Security.Cryptography.X509Certificates;
@@ -13,16 +14,66 @@ namespace RemoteFlightControl.ESP32
 {
     public static class MspMqttClient
     {
+        private static readonly MqttClient InternalClient;
         private static readonly string DeviceId = Guid.NewGuid().ToString();
-        private static readonly X509Certificate HiveCert = new(Resources.GetBytes(Resources.BinaryResources.HiveCert));
-        private static readonly MqttClient InternalClient = new("2184fd25ec7349aca4e1d88598347238.s1.eu.hivemq.cloud", 8883, true, HiveCert, null, MqttSslProtocols.TLSv1_2);
         private static readonly ManualResetEvent ReadySignal = new(false);
+        private static readonly SerialPort MspPort = new("COM2", 115200);
+        private static readonly byte[] RequestBuffer = new byte[256];
+        private static readonly byte[] ResponseHeader = new byte[4];
+        private static readonly byte[] ResponseBuffer = new byte[256];
+
+        static MspMqttClient()
+        {
+            Configuration.SetPinFunction(Gpio.IO16, DeviceFunction.COM2_RX);
+            Configuration.SetPinFunction(Gpio.IO17, DeviceFunction.COM2_TX);
+
+            MspPort.ReadTimeout = 50;
+            MspPort.WriteTimeout = 50;
+            RequestBuffer[0] = (byte)'$';
+            RequestBuffer[1] = (byte)'M';
+            RequestBuffer[2] = (byte)'<';
+
+            X509Certificate cert = new(Resources.GetBytes(Resources.BinaryResources.HiveCert));
+            InternalClient = new MqttClient("2184fd25ec7349aca4e1d88598347238.s1.eu.hivemq.cloud", 8883, true, cert, null, MqttSslProtocols.TLSv1_2)
+            {
+                ProtocolVersion = MqttProtocolVersion.Version_5
+            };
+        }
 
         public static void Start()
         {
+            MspPort.Open();
+            InternalClient.MqttMsgPublishReceived += static delegate(object sender, MqttMsgPublishEventArgs event_args)
+            {
+                try
+                {
+                    string client_id = event_args.Topic.Split('/')[0];
+                    byte[] buffer = event_args.Message;
+                    ResponseBuffer[0] = buffer[0];
+
+                    int response_length = 1;
+                    for(int index = 1; index < buffer.Length;)
+                    {
+                        int length = buffer[index] + 3;
+                        Array.Copy(buffer, index, RequestBuffer, 3, length);
+                        MspPort.Write(RequestBuffer, 0, length + 3);
+                        index += length;
+
+                        MspPort.Read(ResponseHeader, 0, 4);
+                        length = ResponseHeader[3] + 2;
+                        ResponseBuffer[response_length++] = (byte)(length - 2);
+                        MspPort.Read(RequestBuffer, response_length, length);
+                        response_length += length;
+                    }
+
+                    buffer = new byte[response_length];
+                    Array.Copy(ResponseBuffer, 0, buffer, 0, response_length);
+                    InternalClient.Publish($"{client_id}/{DeviceId}/responses", buffer);
+                }
+                catch { }
+            };
             new Thread(ConnectionHandler).Start();
             new Thread(AdvertisingHandler).Start();
-            new Thread(MessageHandler).Start();
 
             [DoesNotReturn] static void ConnectionHandler()
             {
@@ -50,31 +101,13 @@ namespace RemoteFlightControl.ESP32
             [DoesNotReturn] static void AdvertisingHandler()
             {
                 byte[] message = Encoding.UTF8.GetBytes(DeviceId);
-                ArrayList user_properties = new();
                 while(true)
                 {
                     ReadySignal.WaitOne();
                     try
                     {
-                        InternalClient.Publish("NF-FC", message, "", user_properties, MqttQoSLevel.AtLeastOnce, false);
+                        InternalClient.Publish("NF-FC", message, null, null, MqttQoSLevel.AtLeastOnce, false);
                         Thread.Sleep(5000);
-                    }
-                    catch { }
-                }
-            }
-            [DoesNotReturn] static void MessageHandler()
-            {
-                InternalClient.MqttMsgPublishReceived += delegate(object sender, MqttMsgPublishEventArgs event_args)
-                {
-                    MspClient.EnqueueRequest(event_args.Topic.Split('/')[0], event_args.Message);
-                };
-                while(true)
-                {
-                    try
-                    {
-                        ReadySignal.WaitOne();
-                        DictionaryEntry entry = MspClient.DequeueResponse();
-                        InternalClient.Publish($"{entry.Key}/{DeviceId}/responses", (byte[])entry.Value);
                     }
                     catch { }
                 }
--- RemoteFlightControl.ESP32/Program.cs
+++ RemoteFlightControl.ESP32/Program.cs
@@ -1,19 +1,24 @@
 #nullable enable
 
 using System.Threading;
+using System.Diagnostics;
 using nanoFramework.Networking;
+using nanoFramework.Runtime.Native;
 
 namespace RemoteFlightControl.ESP32
 {
-    public class Program
+    public static class Program
     {
         public static void Main()
         {
             WifiNetworkHelper.SetupNetworkHelper("CISPE-PHONE", "11111111");
-            MspClient.Start();
             WifiNetworkHelper.NetworkReady.WaitOne();
             MspMqttClient.Start();
-            Thread.Sleep(Timeout.Infinite);
+            while(true)
+            {
+                Debug.WriteLine($"FreeMemory: {GC.Run(true)}");
+                Thread.Sleep(2000);
+            }
         }
     }
 }
